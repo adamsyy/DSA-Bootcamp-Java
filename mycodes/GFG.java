c930da41b8b292f2f5d9d3cd1df35f9835079039
Adam Oommen Jacob <adamoommenjacob@gmail.com>
2021-09-19 23:55:23 +0530

    gfg

--- mycodes/GFG.java
+++ mycodes/GFG.java
@@ -1,3 +1,4 @@
+
 // importing java libraries.
 import java.util.HashMap;
 import java.util.Iterator;
@@ -6,18 +7,17 @@ import java.util.Map.Entry;
 
 // class for iterating HashMaps.
 public class GFG {
-	public static void main(String[] arguments) {
-		// creating hash_map.
-		Map<Integer, String> hash_map = new HashMap<Integer, String>();
-		// inserting value.
-		hash_map.put(1, "Thor");
-		hash_map.put(2, "Iron man");
-		// setting up iterator.
-		Iterator<Entry<Integer, String>> it = hash_map.entrySet().iterator();
-		// iterating every set of entry in the HashMap. 
-		while (it.hasNext()) {
-			Map.Entry<Integer, String> set = (Map.Entry<Integer, String>) it.next();
-			System.out.println(set.getKey() + " = " + set.getValue());
-		}
-	}
-}
+    public static void main(String[] arguments) {
+        // creating hash_map.
+        Map<String, String> hash_map = new HashMap<>();
+        // inserting value.
+        hash_map.put("1", "Thor");
+        hash_map.put("2", "Iron man");
+        // setting up iterator.
+
+   for(Map.Entry<String,String> entry:hash_map.entrySet()){
+       if(entry.getKey().contains("1")){System.out.println(entry.getKey());}
+       
+   }
+    }
+}   
